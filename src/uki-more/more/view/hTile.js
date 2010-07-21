4fa79962723b24c097de7705bd89181513714b20
Robert Stehwien <robert.stehwien@universalmind.com>
2010-07-21 08:29:03 -0600

    remove hFlow in favor of hTile that can do both

--- src/uki-more/more/view/hTile.js
+++ src/uki-more/more/view/hTile.js
@@ -2,35 +2,73 @@ include('../view.js');
 
 uki.view.declare('uki.more.view.HTile', uki.view.VFlow, function(Base) {
 
-    function getTileGrid(childViews, maxWidth) {
-        var ti = {};
+    function getTileGrid(childViews, maxWidth, isEqualTiles) {
+        var ti = {totalHeight: 0, totalWidth: 0, childRects: []};
 
-        ti.width = uki.reduce(0, childViews, function(max, e) {
-            return (e.visible() && e.width() > max) ? e.width() : max
-        });
+        ti.maxChildWidth = uki.reduce(0, childViews, function(max, e) {
+             return (e.visible() && e.width() > max) ? e.width() : max
+         });
 
-        ti.height = uki.reduce(0, childViews, function(max, e) {
-            return (e.visible() && e.height() > max) ? e.height() : max
-        });
+         ti.maxChildHeight = uki.reduce(0, childViews, function(max, e) {
+             return (e.visible() && e.height() > max) ? e.height() : max
+         });
 
-        ti.across = Math.floor(maxWidth / ti.width);
-        if (ti.across <= 0 && ti.width >= 0) {
-            ti.across = 1
+        // Now layout each item
+        var x = 0, y = 0, rowHeight = 0;
+        var view, rect;
+        for (var i = 0; i < childViews.length; i++) {
+            view = childViews[i];
+
+            // skip invisible
+            if (!view.visible()) {
+                ti.childRects.push(view.x(), view.y(), view.width(), view.height());
+                continue;
+            }
+
+            // check if need to start a new row
+            if ((view.width() + x) > maxWidth && x > 0) {
+                x = 0;
+                y = y + (isEqualTiles ? ti.maxChildHeight : rowHeight);
+                rowHeight = 0;
+            }
+
+            rect = new uki.geometry.Rect(x, y, view.width(), view.height());
+            ti.childRects.push(rect);
+
+            // check if we have achieved a new max width
+            if ((x + rect.width) > ti.totalWidth) {
+               ti.totalWidth = x + rect.width;
+            }
+
+            // check if we have gotten a new row height
+            if (rect.height > rowHeight) {
+               rowHeight = rect.height;
+            }
+
+            // move over a column
+            x = x + (isEqualTiles ? ti.maxChildWidth : view.width());
         }
 
-        ti.down = Math.ceil(childViews.length / ti.across);
+        ti.totalHeight = y + rowHeight;
+
 
         return ti;
     }
 
+    this.isEqualTiles = uki.newProp('_isEqualTiles');
+
+    this._setup = function() {
+        Base._setup.call(this);
+        this._isEqualTiles = true;
+    };
+
     this.contentsSize = function() {
-        var ti = getTileGrid(this._childViews, this.parent().width());
-        return new Size(this.contentsWidth(), ti.height * ti.down);
+        var ti = getTileGrid(this._childViews, this.parent().width(), this._isEqualTiles);
+        return new Size(ti.totalWidth, ti.totalHeight);
     };
 
     this._resizeChildViews = function(oldRect) {
-        var row = 0, col = 0;
-        var ti = getTileGrid(this._childViews, this.parent().width());
+        var ti = getTileGrid(this._childViews, this.parent().width(), this._isEqualTiles);
         var view;
 
         for (var i = 0, childViews = this.childViews(); i < childViews.length; i++) {
@@ -38,22 +76,11 @@ uki.view.declare('uki.more.view.HTile', uki.view.VFlow, function(Base) {
 
             view.parentResized(oldRect, this._rect);
 
-            view.rect().x = col * ti.width;
-            view.rect().y = row * ti.height;
-
-            if (view.visible()) {
-                if (this._hidePartlyVisible) {
-                    view.visible(view._rect.width + ti.width <= this._rect.width);
-                }
-                col++;
-                if (col >= ti.across) {
-                    col = 0;
-                    row++;
-                }
-            }
+            view.rect(ti.childRects[i]);
         }
-        this.rect().width = col * ti.width;
-        this.rect().height = row * ti.height
+
+        this.rect().width = ti.totalWidth;
+        this.rect().height = ti.totalHeight;
     };
 
 });
